07bee4e7b7e819cb81954e4eeb2fc59cb5be1e2c
Christian Isaman <christianisaman@outlook.com>
2025-01-11 16:01:24 +0800

    feat: edit entry

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import util
 import markdown2
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -11,8 +11,28 @@ def entry_list_view():
     return render_template("entry_list.html", entries=util.list_entries())
 
 
+@app.route("/wiki/<entry_title>/edit", methods=["GET", "POST"])
+def entry_edit_view(entry_title):
+    if request.method == "POST":
+        entry_content = request.form["entry_content"]
+        util.save_entry(entry_title, entry_content)
+        return redirect(f"/wiki/{entry_title}")
+    # GET request
+    return render_template(
+        "entry_edit.html",
+        entry_content=util.get_entry(entry_title),
+        entry_title=entry_title,
+    )
+
+
 @app.route("/wiki/<entry_title>")
 def entry_detail_view(entry_title):
     return render_template(
-        "entry_detail.html", entry=markdown2.markdown(util.get_entry(entry_title))
+        "entry_detail.html",
+        entry_content=markdown2.markdown(util.get_entry(entry_title)),
+        entry_title=entry_title,
     )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- util.py
+++ util.py
@@ -1,19 +1,21 @@
 import os
+import re
 
 base_path = os.path.join(os.path.dirname(__file__), "entries")
 
 
-def save_entry(filename, content):
+def save_entry(filename: str, content):
     """
     Save content to a markdown file.
     """
-    filename += ".md"
+    if not filename.endswith(".md"):
+        filename += ".md"
     file_path = os.path.join(base_path, filename)
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(content)
 
 
-def get_entry(filename):
+def get_entry(filename: str):
     """
     Read content from a markdown file.
     """
@@ -31,4 +33,4 @@ def list_entries():
     """
     List all markdown files in the directory.
     """
-    return [f for f in os.listdir(base_path) if f.endswith(".md")]
+    return [re.sub(r"\.md$", "", f) for f in os.listdir(base_path) if f.endswith(".md")]
