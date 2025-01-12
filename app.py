b4bb97c4f7490a318341f374df3ef383b599700f
Christian Isaman <christianisaman@outlook.com>
2025-01-12 09:36:19 +0800

    feat: create entry

--- app.py
+++ app.py
@@ -7,17 +7,34 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
+@app.route("/create/", methods=["GET", "POST"])
+def entry_create_view():
+    """
+    If the form is submitted, save the entry and redirect to the entry
+    detail view; otherwise, render the create form
+    """
+    if request.method == "POST":
+        return handle_entry_form()
+    # GET request
+    return render_template("entry_create.html")
+
+
 @app.route("/")
 def entry_list_view():
+    """
+    Render the list of entries
+    """
     return render_template("entry_list.html", entries=util.list_entries())
 
 
 @app.route("/wiki/<entry_title>/edit", methods=["GET", "POST"])
 def entry_edit_view(entry_title):
+    """
+    If the form is submitted, call helper function to save the entry;
+    otherwise, render the edit form with the entry content
+    """
     if request.method == "POST":
-        entry_content = request.form["entry_content"]
-        util.save_entry(entry_title, entry_content)
-        return redirect(f"/wiki/{entry_title}")
+        return handle_entry_form(entry_title)
     # GET request
     return render_template(
         "entry_edit.html",
@@ -28,6 +45,9 @@ def entry_edit_view(entry_title):
 
 @app.route("/wiki/<entry_title>/")
 def entry_detail_view(entry_title):
+    """
+    Render the entry detail view with the content of the entry
+    """
     return render_template(
         "entry_detail.html",
         entry_content=markdown2.markdown(util.get_entry(entry_title)),
@@ -35,8 +55,24 @@ def entry_detail_view(entry_title):
     )
 
 
+def handle_entry_form(entry_title=None):
+    """
+    Helper function to handle creating or editing an entry
+    """
+    if not entry_title:
+        entry_title = request.form["entry_title"]
+        if util.get_entry(entry_title):
+            return render_template("error.html", message="Entry already exists")
+    entry_content = request.form["entry_content"]
+    util.save_entry(entry_title, entry_content)
+    return redirect(f"/wiki/{entry_title}")
+
+
 @app.route("/random/")
 def random_entry_view():
+    """
+    Redirect to a random entry
+    """
     return redirect(f"/wiki/{random.choice(util.list_entries())}")
 
 
