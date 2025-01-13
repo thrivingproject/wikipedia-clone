d1182f871e3184596742d476f85d9ca6e2e7eebb
Christian Isaman <christianisaman@outlook.com>
2025-01-13 14:55:34 +0800

    style: refactor js

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,19 @@
-document.querySelector(".main-menu-icon").addEventListener("click", function () {
-  document.querySelector(".dropdown-content").classList.toggle("main-menu-show");
-});
+document.querySelector(".main-menu-icon").addEventListener("click", toggleMenu);
+window.addEventListener("click", hideMenu);
 
-window.addEventListener("click", function (event) {
+/**
+ * Toggle the menu when clicking on the menu icon
+ */
+function toggleMenu() {
+  document.querySelector(".dropdown-content").classList.toggle("main-menu-show");
+}
+
+/**
+ * Hide the menu when clicking outside of the menu
+ * @param {*} event
+ */
+function hideMenu(event) {
   if (!event.target.matches(".main-menu-icon")) {
     document.querySelector(".dropdown-content").classList.remove("main-menu-show");
   }
-});
+}
